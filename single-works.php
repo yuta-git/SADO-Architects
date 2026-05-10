c489e697b240d2cf6f931bb2b4166f4e7148de80
yuta-git <yuta6234@gmail.com>
2026-05-10 10:48:14 +0900

    Works詳細ページの作成

--- single-works.php
+++ single-works.php
@@ -7,22 +7,28 @@
 
       <article class="p-works-detail__article">
         <header class="p-works-detail__header">
-          <p class="p-works-detail__category">古民家再生</p>
+          <p class="p-works-detail__category">
+            <?php
+            $terms = get_the_terms($post->ID, 'workscat');
+            foreach ($terms as $term) {
+              echo $term->name;
+            }
+            ?>
+          </p>
           <h2 class="p-works-detail__headline">
-            佐渡の山間に建つ築80年の立派な古民家。古き良き趣はそのままに、耐震性能や温熱性能を住みやすく向上させました。
+            <?php the_title(); ?>
           </h2>
-          <time datetime="20XX-XX-XX" class="p-works-detail__date">20XX.mo.day</time>
+          <time datetime="20XX-XX-XX" class="p-works-detail__date"><?php the_time('Y.m.d'); ?></time>
         </header>
-
         <figure class="p-works-detail__kv">
-          <img
-            src="./img/works/work01.jpg"
-            alt="古民家再生"
-            width="800"
-            height="560"
-            loading="eager" />
+          <?php
+          if (has_post_thumbnail()) :
+            the_post_thumbnail('post-thumbnail');
+          else :
+            echo '<img src="' . get_template_directory_uri() . '/img/works/work01.jpg" alt="古民家再生" width="800" height="560" loading="eager" />';
+          endif;
+          ?>
         </figure>
-
         <div class="p-works-detail__body">
           <p class="p-works-detail__text">
             今回リノベーションする物件は、佐渡の山間に建つ築80年の立派な古民家です。元は農業を営んでいた方が所有していて、古民家と呼ぶに相応しい伝統的な木組の家です。いずれは田舎で暮らすのが夢だったという、今の住まい手。家の購入を考えていたところ、母方の里だった佐渡にちょうど空家がでたので、一念発起して購入されたそうです。古民家と聞いて魅力的に感じる方も多いかもしれません。ですが実際は長年空き家として放置されていることが多く、リフォームやリノベーションをしなければ、まともに住むことができない物件が多いです。その物件によって状態もまちまちのため、まずはじっくりその物件を、実調するところから始まります。再生可能な古民家か？住まい手の想いを実現できるか？木材に腐朽や蟻害（シロアリ被害）がないか？などといった視点でじっくりと確認し判断します。今回の古民家は、昭和の家とは思えないくらい柱や梁も立派で丈夫な建物だったので、住まい手のご希望の通り、古き良き趣を残したままリノベーションすることとしました。ただし、内部は座敷の続きの部屋で、暗く寒い状態だったので、耐震性能と温熱性能の両方を向上させる設計としました。
@@ -32,7 +38,7 @@
         <div class="p-works-detail__images">
           <figure class="p-works-detail__image">
             <img
-              src="./img/works/work01.jpg"
+              src="<?php echo get_template_directory_uri(); ?>/img/works/work01.jpg"
               alt=""
               width="500"
               height="380"
@@ -40,7 +46,7 @@
           </figure>
           <figure class="p-works-detail__image">
             <img
-              src="./img/works/work01.jpg"
+              src="<?php echo get_template_directory_uri(); ?>/img/works/work01.jpg"
               alt=""
               width="500"
               height="380"
@@ -50,18 +56,26 @@
 
         <div class="p-works-detail__body">
           <p class="p-works-detail__text">
-            大きな間取りはそのままに、来客のための土間スペースを玄関近くに設け、水回りも現在の暮らしに合わせて修繕しました。また、四方を山に囲まれた景色の良いところなので、すべての部屋から山が見えるように窓を開けました。耐震性能と温熱性能の両方を向上させ、古民家と言えども、地震に強くあたたかい家をつくることができました。改修前は、外気と室内の温度が変わらぬほど、寒い家でしたが、改修後はとても暖かく暮らしやすいと嬉しいお言葉をいただいております。念願だった田舎暮らしも、敷地内の場所で野菜を育てたり、地域の活動に参加したりと充実した時間を過ごしておられるそうです。
+            大きな間取りはそのままに、来客のための土間スペースを玄関近くに設け、水回りも現在の暮らしに合わせて修繕しました。また、四方を山に囲まれた景色の良いところなので、すべての部屋から山が見えるように窓を開けました。耐震性能と温熱性能の両方を向上させ、古民家と言えども、地震に強くてあたたかい家をつくることができました。改修前は、外気と室内の温度が変わらぬほど、寒い家でしたが、改修後はとても暖かく暮らしやすいと嬉しいお言葉をいただいております。念願だった田舎暮らしも、敷地内の場所で野菜を育てたり、地域の活動に参加したりと充実した時間を過ごしておられるそうです。
           </p>
         </div>
 
         <nav class="c-pager" aria-label="作品詳細ページ送り">
-          <a href="#" class="c-pager__link c-pager__link--prev">
-            <span class="c-pager__arrow">←</span> BACK
-          </a>
-          <a href="works.html" class="c-pager__link c-pager__link--all">ALL</a>
-          <a href="#" class="c-pager__link c-pager__link--next">
-            NEXT <span class="c-pager__arrow">→</span>
-          </a>
+          <?php
+          $prev_post = get_previous_post();
+          $next_post = get_next_post();
+          ?>
+          <?php if ($prev_post) : ?>
+            <a href="<?php echo esc_url(get_permalink($prev_post)); ?>" class="c-pager__link c-pager__link--prev">
+              <span class="c-pager__arrow">←</span> BACK
+            </a>
+          <?php endif; ?>
+          <a href="<?php echo esc_url(get_post_type_archive_link('works')); ?>" class="c-pager__link c-pager__link--all">ALL</a>
+          <?php if ($next_post) : ?>
+            <a href="<?php echo esc_url(get_permalink($next_post)); ?>" class="c-pager__link c-pager__link--next">
+              NEXT <span class="c-pager__arrow">→</span>
+            </a>
+          <?php endif; ?>
         </nav>
       </article>
     </div>
